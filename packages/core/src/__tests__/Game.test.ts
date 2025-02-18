813bb3c819daa865c1c6f5de648ed91875f2b0b1
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-02-18 20:43:59 -0300

    test: add test to prevent adding a game at the same place and time

--- packages/core/src/__tests__/Game.test.ts
+++ packages/core/src/__tests__/Game.test.ts
@@ -1,45 +1,81 @@
-import { IGamesRepository, Game, IGames, EStateGame,  } from "../games";
+import { IGamesRepository, Game, IGames, EStateGame } from "../games";
 import { ECategoryTeam, ETypeTeam } from "../teams";
 const mockRepo: jest.Mocked<IGamesRepository> = {
-    getGames: jest.fn(),
-    addGame: jest.fn(),
-    getGameById: jest.fn(),
-    getGamesFromCategory: jest.fn(),
-    getGamesFromType: jest.fn(),
-    getGamesFromDate: jest.fn(),
-    getGamesFromTeam: jest.fn(),
-    updateGame: jest.fn(),
-    updateState: jest.fn(),
+  getGames: jest.fn(),
+  addGame: jest.fn(),
+  getGameById: jest.fn(),
+  getGamesFromCategory: jest.fn(),
+  getGamesFromType: jest.fn(),
+  getGamesFromDate: jest.fn(),
+  getGamesFromTeam: jest.fn(),
+  updateGame: jest.fn(),
+  updateState: jest.fn(),
 };
 const mockGame: IGames = {
-    id: "1",
-    team1: { id: "t1", name: "Time 1", category: "SUB15MEN" as ECategoryTeam, type: "FUTSAL" as ETypeTeam, group: "A" },
-    team2: { id: "t2", name: "Time 2", category: "SUB15MEN" as ECategoryTeam, type: "FUTSAL" as ETypeTeam, group: "A" },
-    date: new Date(),
-    time: 15,
-    location: "Estádio 1",
-    state: "AGUARDANDO" as EStateGame,
-  };
+  id: "1",
+  team1: {
+    id: "t1",
+    name: "Time 1",
+    category: "SUB15MEN" as ECategoryTeam,
+    type: "FUTSAL" as ETypeTeam,
+    group: "A",
+  },
+  team2: {
+    id: "t2",
+    name: "Time 2",
+    category: "SUB15MEN" as ECategoryTeam,
+    type: "FUTSAL" as ETypeTeam,
+    group: "A",
+  },
+  date: new Date(),
+  time: 15,
+  location: "Estádio 1",
+  state: "AGUARDANDO" as EStateGame,
+};
 describe("Game class", () => {
-    let gameService: Game;
+  let gameService: Game;
 
-    beforeEach(() => {
-      gameService = new Game(mockRepo);
-    });
-    it("should add a game successful", async () => {
-        mockRepo.getGames.mockResolvedValue([]);
-        mockRepo.addGame.mockResolvedValue(mockGame);
-    
-        const newGame: Partial<IGames> = {
-          id: "1",
-          team1: { id: "t1", name: "Time 1", category: "SUB15MEN" as ECategoryTeam, type: "FUTSAL" as ETypeTeam, group: "A" },
-          team2: { id: "t2", name: "Time 2", category: "SUB15MEN" as ECategoryTeam, type: "FUTSAL" as ETypeTeam, group: "A" },
-          date: new Date(),
-          time: 15,
-          location: "Estádio 1",
-          state: "AGUARDANDO" as EStateGame,
-        };
-    
-        await expect(gameService.addGame(newGame)).resolves.toEqual(mockGame);
-      })
+  beforeEach(() => {
+    gameService = new Game(mockRepo);
+  });
+  it("should add a game successful", async () => {
+    mockRepo.getGames.mockResolvedValue([]);
+    mockRepo.addGame.mockResolvedValue(mockGame);
+
+    const newGame: Partial<IGames> = {
+      id: "1",
+      team1: {
+        id: "t1",
+        name: "Time 1",
+        category: "SUB15MEN" as ECategoryTeam,
+        type: "FUTSAL" as ETypeTeam,
+        group: "A",
+      },
+      team2: {
+        id: "t2",
+        name: "Time 2",
+        category: "SUB15MEN" as ECategoryTeam,
+        type: "FUTSAL" as ETypeTeam,
+        group: "A",
+      },
+      date: new Date(),
+      time: 15,
+      location: "Estádio 1",
+      state: "AGUARDANDO" as EStateGame,
+    };
+
+    await expect(gameService.addGame(newGame)).resolves.toEqual(mockGame);
+  });
+  it("should prevent adding a game at the same place and time", async () => {
+    mockRepo.getGames.mockResolvedValue([
+      { location: "Estádio 1", time: 15 } as IGames,
+    ]);
+
+    const newGame: Partial<IGames> = {
+      location: "Estádio 1",
+      time: 15,
+    };
+
+    await expect(gameService.addGame(newGame)).rejects.toThrow("Já existe um jogo neste local e horário");
+  });
 });
