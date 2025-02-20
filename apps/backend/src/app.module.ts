6d9ac68b9a5a7248beb84b0b420e4d02b4223069
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-02-20 07:24:47 -0300

    feat: import dbModule in the app module

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DbModule } from './database/db.module';
 
 @Module({
-  imports: [],
+  imports: [DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
