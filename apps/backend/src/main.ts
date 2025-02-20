aa94ed9f9513a4a4cff74404cbb6c39c0e80098e
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-02-20 07:49:59 -0300

    refactor: improve main

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ErrorFilter } from './app.error.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalFilters(new ErrorFilter());
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
-bootstrap();
+void bootstrap();
