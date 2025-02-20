ab59a9a912b42f87bc87ac8843cc770ea3ba4109
Luan Alves <teste.checklist@nossoatacarejo.com.br>
2025-02-20 08:01:24 -0300

    refactor: delete app service provider

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,12 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './database/db.module';
 
 @Module({
   imports: [DbModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule { }
